0412f1f265a9a21479e173519ebfd03467eb45ef
Alexandr Rodionov <alexrodionby@gmail.com>
2022-06-12 13:20:41 +0300

    feat(app): added custom buttons to the start screen

---
+++ VKProject/Extensions/CustomButtons/TwoLinedButton.swift
@@ -0,0 +1,61 @@
+//
+//  TwoLinedButton.swift
+//  VKProject
+//
+//  Created by Alexandr Rodionov on 8.06.22.
+//
+
+import UIKit
+
+struct TwoLinedButtonViewModel {
+    let primaryText: String
+    let secondaryText: String
+}
+
+final class TwoLinedButton: UIButton {
+    
+    private let primaryLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }()
+    
+    private let secondaryLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(primaryLabel)
+        addSubview(secondaryLabel)
+        clipsToBounds = true
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        backgroundColor = .systemBlue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        primaryLabel.frame = CGRect(x: 5, y: 0, width: frame.size.width - 10, height: frame.size.height / 2)
+        secondaryLabel.frame = CGRect(x: 5, y: frame.size.height / 2, width: frame.size.width - 10, height: frame.size.height / 2)
+    }
+    
+    func configure(with viewModel: TwoLinedButtonViewModel) {
+        primaryLabel.text = viewModel.primaryText
+        secondaryLabel.text = viewModel.secondaryText
+    }
+    
+}
